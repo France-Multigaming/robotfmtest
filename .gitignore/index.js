ea6fd0f63adb49c247c663613363079b7b302f5c
France-Multigaming <53142230+France-Multigaming@users.noreply.github.com>
2019-08-31 11:27:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,31 +8,29 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-fs.readdir("./Commandes/", (error, f) => {
+fs.readdir('./Commandes/', (error, f) => {
     if(error) console.log(error);
 
-    let commandes = f.filter(f => f.split(".").pop() === "js");
-    if(commandes.length <= 0) return console.log("Aucune commande trouvé !");
+        let commandes = f.filter(f => f.split(".").pop() === "js");
+        if(commandes.length <= 0) return console.log("Aucune commande trouvé !");
 
-    commandes.forEach((f) => {
+        commandes.forEach((f) => {
 
-        let commande = require(`./Commandes/${f}`);
-        console.log(`${f} commande chargée !`);
-
-    client.commands.set(commande.help.name, commande);
-    });
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
 });
 
 fs.readdir("./Events/", (error, f) => {
     if(error) console.log(error);
-    console.log(`${f.length} event en chargement`);
-    f.forEach((f) => {
-        const events = require(`./Events/${f}`);
-        const event = f.split(".")[0];
-
-    client.on(event, events.bind(null, client));
-
-    });
+        console.log(`${f.length} event en chargement`);
+    
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split(".")[0];
+            client.on(event, events.bind(null, client));
+        });
 });
 
 // Welcome message
