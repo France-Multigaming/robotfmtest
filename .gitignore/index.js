9a933909267d2bc4cb907d0f0de3263af1c18789
France-Multigaming <53142230+France-Multigaming@users.noreply.github.com>
2019-09-01 08:56:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,37 @@ const Discord = require("discord.js");
 // Client instance
 var client = new Discord.Client();
 
+//Fs
+const fs = require('fs');
 
+client.commands = new Discord.Collection();
+
+fs.readdir("./Commandes/", (error, f) => {
+    if(error) console.log(error);
+
+    let commandes = f.filter(f => f.split(".").pop() === "js");
+    if(commandes.length <= 0) return console.log("Aucune commande trouvé !");
+
+    commandes.forEach((f) => {
+
+        let commande = require(`./Commandes/${f}`);
+        console.log(`${f} commande chargée !`);
+
+    client.commands.set(commande.help.name, commande);
+    });
+});
+
+fs.readdir("./Events/", (error, f) => {
+    if(error) console.log(error);
+    console.log(`${f.length} event en chargement`);
+    f.forEach((f) => {
+        const events = require(`./Events/${f}`);
+        const event = f.split(".")[0];
+
+    client.on(event, events.bind(null, client));
+
+    });
+});
 
 // Welcome message
    client.on("guildMemberAdd", user =>{
